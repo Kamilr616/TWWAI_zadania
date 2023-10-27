f1c7276c3ee22f8172fca14346a428b28a741b80
Kamil Rataj <kamilr616@gmail.com>
2023-10-27 10:14:32 +0200

    LAB3 - rozwiązanie zadania 5

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
 const questions = require('./questions').questions;
+const serverPort = require('./config').serverPort;
 
+
+app.listen(serverPort, function () {
+    console.info(`Server is running at port ` + serverPort);
+});
 app.get('/api/questions', (request, response) => {
     response.send(questions);
 });
 
-app.listen(3000, function () {
-    console.info(`Server is running at port 3000`);
+app.get('/api/questions/:id', (request, response) => {
+    const questionId = parseInt(request.params.id);
+
+    if (Number.isInteger(questionId) && questionId >= 0 && questionId < questions.length) {
+        response.json(questions[questionId]);
+    } else {
+        response.status(404).json({ error: 'Question ' + questionId + ' not found!' });
+    }
 });
