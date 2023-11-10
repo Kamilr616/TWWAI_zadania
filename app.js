fb370e978361693b6ecea1ac9e4cb5779a1d02e1
Kamil Rataj <kamilr616@gmail.com>
2023-11-10 11:24:43 +0100

    zadanie 5

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 const express = require('express');
-const serverPort = require('./config').serverPort;
+const config = require('./config');
 const app = express();
+const service = require('./service');
 
 let chart1 = {"type":"line","data":{"labels":["January","February","March","April","May","June"],"datasets":[{"label":"My First dataset","backgroundColor":"rgb(255, 99, 132)","borderColor":"rgb(255, 99, 132)","data":[0,10,5,2,20,30,45]}]},"options":{}};
+
+const temp = `Czas,Temperatura
+2023-11-09 10:00:00,10
+2023-11-09 11:00:00,13
+2023-11-09 12:00:00,16
+2023-11-09 13:00:00,18
+2023-11-09 14:00:00,19
+2023-11-09 15:00:00,19
+2023-11-09 16:00:00,17`;
+
+const dataChart2 = service.convertData(temp);
+
+const chart2 = JSON.stringify(dataChart2);
 
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', (request, response) => {
-    response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu', chart1: JSON.stringify(chart1)})
+    response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu', chart1: JSON.stringify(chart1), chart2})
 });
 
 
 
 
-app.listen(serverPort, function () {
-    console.info(`Server is running at port ${serverPort} `);
+app.listen(config.serverPort, function () {
+    console.info(`Server is running at port ${config.serverPort} `);
 });
 
