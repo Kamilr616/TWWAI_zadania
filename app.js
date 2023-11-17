a0e9749975233bae98b1a2dd3f44cb977aeacde8
Kamil Rataj <kamilr616@gmail.com>
2023-11-17 11:53:06 +0100

    LAB5 - całe

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
 const express = require('express');
 const config = require('./config');
 const app = express();
-const service = require('./service');
+const path = require('path');
 
-let chart1 = {"type":"line","data":{"labels":["January","February","March","April","May","June"],"datasets":[{"label":"Wykres czasu","backgroundColor":"rgb(255, 99, 132)","borderColor":"rgb(255, 99, 132)","data":[0,10,5,2,20,30,45]}]},"options":{}};
-
-const temp = `Czas,Temperatura
-2023-11-09 10:00:00,10
-2023-11-09 11:00:00,13
-2023-11-09 12:00:00,16
-2023-11-09 13:00:00,18
-2023-11-09 14:00:00,19
-2023-11-09 15:00:00,19
-2023-11-09 16:00:00,17`;
-
-const dataChart2 = service.convertData(temp);
-
-const chart2 = JSON.stringify(dataChart2);
-
-const products = [
-    { name: 'Laptop', price: 1000 },
-    { name: 'Smartphone', price: 500 },
-    { name: 'Tablet', price: 300 }
-];
-
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './views');
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 
 app.get('/', (request, response) => {
-    response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu', chart1: JSON.stringify(chart1), chart2, products})
+    response.render(__dirname + '/index.html', {subject: 'Technologie webowe w aplikacjach Internetu'})});
+
+app.get('/template/:variant/:a/:b', (request, res) => {
+    const _variant = request.params.variant;
+    const _a = parseFloat(request.params.a);
+    const _b = parseFloat(request.params.b);
+    let result = 0;
+
+    switch (_variant) {
+        case 'sum':
+            result = _a + _b;
+            break;
+        case 'sub':
+            result = _a - _b;
+            break;
+        case 'multiply':
+            result = _a * _b;
+            break;
+        case 'divide':
+            result = _a / _b;
+            break;
+        default:
+            result = 'Invalid operation';
+    }
+
+    res.render('template', {subject: 'Technologie webowe w aplikacjach Internetu - Kalkulator', _variant, _a, _b, result });
 });
-
-
-
 
 app.listen(config.serverPort, function () {
     console.info(`Server is running at port ${config.serverPort} `);
 });
-
